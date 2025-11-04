efde5731ee306cb81294d2056cc7842388ab312c
TANAY LOKHANDE <tanaylokhande922@gmail.com>
2025-11-04 21:30:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-803137937-55de9",
+  "appId": "1:909276731047:web:ec4282258acff6a8d3ed82",
+  "apiKey": "AIzaSyBcLpqye_LGcV4TLE-wEhFN24GUGELwyc4",
+  "authDomain": "studio-803137937-55de9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "909276731047"
+};
