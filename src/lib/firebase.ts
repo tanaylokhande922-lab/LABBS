5b2fb21fcdb0c20eab63a1dbd78ca58f16c519fb
TANAY LOKHANDE <tanaylokhande922@gmail.com>
2025-11-04 21:10:59 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  projectId: 'studio-803137937-55de9',
+  appId: '1:909276731047:web:ec4282258acff6a8d3ed82',
+  apiKey: 'AIzaSyBcLpqye_LGcV4TLE-wEhFN24GUGELwyc4',
+  authDomain: 'studio-803137937-55de9.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '909276731047',
+  storageBucket: 'studio-803137937-55de9.appspot.com'
 };
 
 // Initialize Firebase for SSR
